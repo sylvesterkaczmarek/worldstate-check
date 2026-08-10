e27edb21c008e023fd01dfe185dd6d810912e776
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-10 10:50:47 +0100

    Fix package version reporting

--- src/worldstate_check/__init__.py
+++ src/worldstate_check/__init__.py
@@ -1,3 +1,8 @@
 """WorldState Check package."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("worldstate-check")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
